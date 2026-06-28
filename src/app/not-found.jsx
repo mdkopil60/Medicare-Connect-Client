b89651c6d9bfa8046329ead258f67226833408d1
KOPIL UDDIN <kopil7132@gmail.com>
2026-06-29 00:13:51 +0600

    update server

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -8,21 +8,17 @@ export default function NotFound() {
             <div className="text-[150px]">
                 🚑
             </div>
-
             {/* Error Message */}
             <h1 className="text-6xl font-bold text-blue-600">
                 404
             </h1>
-
             <h2 className="text-3xl font-semibold mt-4">
                 Page Not Found
             </h2>
-
             <p className="text-gray-500 mt-3 text-center max-w-md">
                 Sorry, the page you are looking for does not exist
                 or has been moved.
             </p>
-
             {/* Back Home Button */}
             <Link
                 href="/"
