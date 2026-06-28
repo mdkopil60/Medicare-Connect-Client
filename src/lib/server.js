39d9e1c1b3205dc12f639ee79746e3d57c570ab0
KOPIL UDDIN <kopil7132@gmail.com>
2026-06-28 23:53:55 +0600

    Update and Redrploy

--- src/lib/server.js
+++ src/lib/server.js
@@ -1,8 +1,6 @@
 export const authHeader = async () => {
     const token = await getUserToken();
 
-    // টোকেন না থাকলে সরাসরি একটি খালি অবজেক্ট রিটার্ন করুন। 
-    // এতে Authorization হেডারটি তৈরিই হবে না।
     if (!token) {
         return {};
     }
@@ -17,7 +15,6 @@ export const serverMutation = async (path, data, method = 'POST') => {
         ...await authHeader()
     };
 
-    // এখানে খেয়াল করুন: ${baseUrl}${path} - মাঝের স্পেসটি সরিয়ে দিয়েছি
     const res = await fetch(`${baseUrl}${path}`, {
         method: method,
         headers: headers,
